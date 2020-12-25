3e9e340ffdc5e268b9c87d2e4b3c4ec115974247
davidchung29 <60008214+dvjr2018@users.noreply.github.com>
2020-12-25 15:05:09 -0800

    new firebase listen for updates

--- Chat/Controller/ChatViewController.swift
+++ Chat/Controller/ChatViewController.swift
@@ -28,9 +28,13 @@ class ChatViewController: UIViewController {
 
     }
     func loadMessages(){
-        messages = []
         
-        db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+            
+            self.messages = []
+            
             if let e = error{
                 print("issue rertrieving data \(e)")
             }else{
@@ -53,7 +57,7 @@ class ChatViewController: UIViewController {
     @IBAction func sendPressed(_ sender: UIButton) {
         
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { (error) in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
                 if let e = error{
                     print("issue saving data to firestore, \(e)")
                 }else{
--- Chat/Controller/RegisterViewController.swift
+++ Chat/Controller/RegisterViewController.swift
@@ -27,3 +27,4 @@ class RegisterViewController: UIViewController {
     }
     
 }
+
